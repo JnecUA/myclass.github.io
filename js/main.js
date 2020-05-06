c9352c512d87841c1476d724abd67c133c1c0704
Maksim <woodoomax123@gmail.com>
2020-05-06 13:21:05 +0300

    Здесь я сделал Секцию 'Мой класс', поменя секцию 'Моя школа' и изменил шапку

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function() {
+    $(window).on("scroll resize", function() {
+        var o = $(window).scrollTop() / ($(document).height() - $(window).height());
+        $(".progress-bar").css({
+            "width": (100 * o | 0) + "%"
+        });
+    })
+});
+$(function(){
+    $("a[href^='#']").click(function(){
+            var _href = $(this).attr("href");
+            $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+            return false;
+    });
+});
+//Created by Maksim Klimenkov
